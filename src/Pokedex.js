efc2f9fd34d6430df5b26e41164826af59128cd9
John Lawver <johnlawver@gmail.com>
2021-03-14 22:16:34 -0700

    ADd Primative Cards

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,7 +1,40 @@
+import { AppBar, Toolbar, Grid, Card, CardContent } from "@material-ui/core";
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  pokedexContianer: {
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
+  },
+});
+
+const getPokemonCard = (info) => {
+  return (
+    <Grid item xs={12} sm={4}>
+      <Card>
+        <CardContent>{info}</CardContent>
+      </Card>
+    </Grid>
+  );
+};
 
 const Pokedex = () => {
-  return <div>this is the Pokedex Page</div>;
+  const { pokedexContianer } = useStyles();
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>This is for sure a nav bar</Toolbar>
+      </AppBar>
+      <Grid container spacing={2} className={pokedexContianer}>
+        {getPokemonCard("Card 1")}
+        {getPokemonCard("Card 2")}
+        {getPokemonCard("Card 3")}
+        {getPokemonCard("Weird Fish")}
+      </Grid>
+    </>
+  );
 };
 
 export default Pokedex;
